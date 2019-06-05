57003786f04db26f5093806c86dbe6fe74dacf3f
Костин Александр Николаевич <alexandr.kostin@info-pro.ru>
2019-06-05 10:06:53 +0400

    Добавьте файлы проекта.

---
+++ Client/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class User
+    {
+
+        public string ID { get; set; }
+        public byte[] Photo { get; set; }
+        public string Name { get; set; }
+
+    }
+}
