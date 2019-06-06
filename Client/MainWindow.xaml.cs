ab70e6122f417370cbc1c11f95a7bb136e625310
Костин Александр Николаевич <alexandr.kostin@info-pro.ru>
2019-06-06 15:48:09 +0400

    15:47 06.06.2019
    15:47 06.06.2019
    Разбил на два окна чат

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -23,10 +23,33 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+
+            chatTextBlock.Inlines.Add(new Bold(new Run("Первое сообщение") { FontWeight = FontWeights.Bold}));
+            chatTextBlock.Inlines.Add(new LineBreak());
+            chatTextBlock.Inlines.Add(new LineBreak());
+            chatTextBlock.Inlines.Add(new Run("Второе сообщение ") { Foreground = Brushes.Blue });
+            chatTextBlock.Inlines.Add(new LineBreak());
+            chatTextBlock.Inlines.Add(new LineBreak());
+            chatTextBlock.Inlines.Add(new Bold(new Run("Третье сообщение") { FontWeight = FontWeights.Bold }));
+            chatTextBlock.Inlines.Add(new LineBreak());
+            chatTextBlock.Inlines.Add(new LineBreak());
+            chatTextBlock.TextAlignment = TextAlignment.Right;
+            chatTextBlock.Inlines.Add(new Run("Четвертое сообщение "));
+            chatTextBlock.Inlines.Add(new LineBreak());
+            chatTextBlock.Inlines.Add(new LineBreak());
+            chatTextBlock.Inlines.Add(new Italic(new Run(":)))")));
+            chatTextBlock.Inlines.Add(new LineBreak());
+            chatTextBlock.Inlines.Add(new LineBreak());
+            chatTextBlock.TextAlignment = TextAlignment.Left;
         }
 
+        private void bSend_Click(object sender, RoutedEventArgs e)
 
-
-
+        {
+            chatTextBlockSend.Inlines.Add(new LineBreak());
+            chatTextBlockSend.Inlines.Add(new LineBreak());
+            chatTextBlockSend.TextAlignment = TextAlignment.Right;
+            chatTextBlockSend.Inlines.Add(new Run(tbMessage.Text));
+        }
     }
 }
